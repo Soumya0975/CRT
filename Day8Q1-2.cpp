b52b6555b843f17fbe935f7cdd303f0a484384b7
Soumya0975 <151668694+Soumya0975@users.noreply.github.com>
2026-06-04 10:29:26 +0530

    Clarify comment for prime number generation
    Updated comment to clarify the purpose of the code.

---
+++ Day8Q1-2.cpp
@@ -0,0 +1,13 @@
+//generate a list of prime numbers with given range use loop range 1-20 leetcode
+#include<iostream>
+using namespace std
+int main(){
+for(int n=1;n<20;i++){
+int count==0;
+}
+for(int i=1;i<num;i++)
+if(n%i==0)
+
+}
+
+add an element at the beginning of the listuse list slicing or insert() method leetcode
